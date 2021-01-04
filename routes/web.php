9ab66f0aef25825db55ce50059981dc05680bd0c
naumura <yutaro0616@gmail.com>
2021-01-05 00:19:37 +0900

    initial_commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'DiaryController@index')->name('diary.index');
+Route::get('diary/create', 'DiaryController@create')->name('diary.create'); // 投稿画面
+Route::post('diary/create', 'DiaryController@store')->name('diary.create'); // 保存処理
+Route::delete('diary/{id}/delete', 'DiaryController@destroy')->name('diary.destroy'); // 削除処理
+Route::get('diary/{id}/edit', 'DiaryController@edit')->name('diary.edit'); // 編集画面
+Route::put('diary/{id}/update', 'DiaryController@update')->name('diary.update'); //更新処理
