240ce4e3201cd00b6b8304c10b0feb8b35a4d127
naumura <yutaro0616@gmail.com>
2021-01-05 22:46:45 +0900

    add_layout.blade.php and templeted

--- resources/views/diaries/edit.blade.php
+++ resources/views/diaries/edit.blade.php
@@ -9,35 +9,29 @@
     <title>編集画面</title>
 </head>
 <body>
-    <section class="container m-5">
-        <div class="row justify-content-center">
-            <div class="col-8">
-                @if($errors->any())
-                    <ul>
-                        @foreach($errors->all() as $message)
-                        <li class="alert alert-danger">{{ $message }}</li>
-                        @endforeach
-                    </ul>
-                @endif
-                <form action="{{ route('diary.update', ['id' => $diary->id]) }}" method="post">
-                    @csrf
-                    @method('put')
-                    <div class="form-group">
-                        <label for="title">タイトル</label>
-                        <input type="text" class="form-control" name="title" id="title" value="{{ old('title', $diary->title) }}">
-                    </div>
-                    <div class="form-group">
-                        <label for="title">本文</label>
-                        <textarea class="form-control" name="body" id="body">{{ old('body', $diary->body) }}</textarea>
-                    </div>
-                    <div class="text-right">
-                        <button type="submit" class="btn btn-primary">更新</button>
-                    </div>
-                </form>
-            </div>
+@extends('layout')
+
+@section('title')
+一覧
+@endsection
+
+@section('content')
+<a href="{{ route('diary.create') }}" class="btn btn-primary btn-block">
+    新規投稿
+</a>
+    @foreach ($diaries as $diary)
+        <div class="m-4 p-4 border border-primary">
+            <p>{{ $diary->title }}</p>
+            <p>{{ $diary->body }}</p>
+            <p>{{ $diary->created_at }}</p>
+            <a class="btn btn-success" href="{{ route('diary.edit', ['id' => $diary->id]) }}">編集</a>
+            <form action="{{ route('diary.destroy', ['id' => $diary->id]) }}" method="post" class="d-inline">
+                @csrf
+                @method('delete')
+                <button class="btn btn-danger">削除</button>
+            </form>
         </div>
-    </section>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    @endforeach
+    @endsection
 </body>
 </html>
