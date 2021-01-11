13eefa1adce6c44ae4304a09ae716f288987d01b
SasanoHayato <sasano.hayato@gmail.com>
2021-01-12 01:02:28 +0800

    modify

--- resources/views/diaries/create.blade.php
+++ resources/views/diaries/create.blade.php
@@ -16,22 +16,26 @@
 @endsection
 
 @section('content')
-<a href="{{ route('diary.create') }}" class="btn btn-primary btn-block">
-    新規投稿
-</a>
-    @foreach ($diaries as $diary)
-        <div class="m-4 p-4 border border-primary">
-            <p>{{ $diary->title }}</p>
-            <p>{{ $diary->body }}</p>
-            <p>{{ $diary->created_at }}</p>
-            <a class="btn btn-success" href="{{ route('diary.edit', ['id' => $diary->id]) }}">編集</a>
-            <form action="{{ route('diary.destroy', ['id' => $diary->id]) }}" method="post" class="d-inline">
+<section class="container m-5">
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <form action="{{ route('diary.create') }}" method="POST">
                 @csrf
-                @method('delete')
-                <button class="btn btn-danger">削除</button>
+                <div class="form-group">
+                    <label for="title">タイトル</label>
+                    <input type="text" class="form-control" name="title" id="title" />
+                </div>
+                <div class="form-group">
+                    <label for="body">本文</label>
+                    <textarea class="form-control" name="body" id="body"></textarea>
+                </div>
+                <div class="text-right">
+                    <button type="submit" class="btn btn-primary">投稿</button>
+                </div>
             </form>
         </div>
-    @endforeach
-    @endsection
+    </div>
+</section>
+@endsection
 </body>
 </html>
--- resources/views/diaries/edit.blade.php
+++ resources/views/diaries/edit.blade.php
@@ -16,22 +16,34 @@
 @endsection
 
 @section('content')
-<a href="{{ route('diary.create') }}" class="btn btn-primary btn-block">
-    新規投稿
-</a>
-    @foreach ($diaries as $diary)
-        <div class="m-4 p-4 border border-primary">
-            <p>{{ $diary->title }}</p>
-            <p>{{ $diary->body }}</p>
-            <p>{{ $diary->created_at }}</p>
-            <a class="btn btn-success" href="{{ route('diary.edit', ['id' => $diary->id]) }}">編集</a>
-            <form action="{{ route('diary.destroy', ['id' => $diary->id]) }}" method="post" class="d-inline">
+<section class="container m-5">
+    <div class="row justify-content-center">
+        <div class="col-8">
+            @if($errors->any())
+                <ul>
+                    @foreach($errors->all() as $message)
+                    <li class="alert alert-danger">{{ $message }}</li>
+                    @endforeach
+                </ul>
+            @endif
+            <form action="{{ route('diary.update', ['id' => $diary->id]) }}" method="post">
                 @csrf
-                @method('delete')
-                <button class="btn btn-danger">削除</button>
+                @method('put')
+                <div class="form-group">
+                    <label for="title">タイトル</label>
+                    <input type="text" class="form-control" name="title" id="title" value="{{ old('title', $diary->title) }}">
+                </div>
+                <div class="form-group">
+                    <label for="title">本文</label>
+                    <textarea class="form-control" name="body" id="body">{{ old('body', $diary->body) }}</textarea>
+                </div>
+                <div class="text-right">
+                    <button type="submit" class="btn btn-primary">更新</button>
+                </div>
             </form>
         </div>
-    @endforeach
-    @endsection
+    </div>
+</section>
+@endsection
 </body>
 </html>
